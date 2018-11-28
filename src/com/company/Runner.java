6a76aee5f1439b3f44cab1c6790759bc5e0e6cba
rcooper3307 <rcooper3307@bths.edu>
2018-11-28 08:41:32 -0500

    sd

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -4,5 +4,7 @@ public class Runner {
 
     public static void main(String[] args) {
 	// write your code here
+        String[][] face = new String[5][8];
+        FaceDrawing face1 = new FaceDrawing(face);
     }
 }
